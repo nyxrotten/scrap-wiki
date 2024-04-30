c97cdbd84d4c6d6ab8dbf14b045813b08d5d38c7
nyxrotten <nes_calina@hotmail.com>
2024-04-30 10:30:33 +0200

    feat: links filter alphabetical

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const url = "https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_heavy_metal";
+
+
+
+app.get("/", (req, res) => {
+    
+    axios.get(url).then((response) => {
+        if(response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            //const pageTitle = $("title").text();
+
+            const links = [];
+            $(".external").each((index, element) => {
+                const link = $(element).attr("href")
+                if (link.includes("from=")) {
+                    links.push(link)
+                }
+                
+                
+            })
+            console.log(links)
+
+
+            
+        }
+    })
+
+   
+    
+})
+
+app.listen(3000, () => {
+    console.log("Express está escuchando en el puerto 3000")
+})
